1031918ac59b612f4d1e0dcd8486d9e0acf3158d
François Prunayre <fx.prunayre@gmail.com>
2020-11-10 11:40:53 +0100

    Update migrate-3104-othu.sql

--- web/src/main/webapp/WEB-INF/classes/setup/sql-gl/migrate-3104-othu.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-gl/migrate-3104-othu.sql
@@ -79,7 +79,7 @@ INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system
 
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/users/identicon', 'gravatar:mp', 0, 9110, 'n');
 
-UPDATE Settings SET value='3.10.4' WHERE name='system/platform/version';
+UPDATE Settings SET value='3.10.5' WHERE name='system/platform/version';
 UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
 
 
